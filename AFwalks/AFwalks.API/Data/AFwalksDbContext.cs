c5101efcbcc52cf8cfd33386d8ae00e7c7bde07d
clincaruto <clindrop@gmail.com>
2023-02-28 14:30:45 +0100

    End of Section 2

---
+++ AFwalks/AFwalks.API/Data/AFwalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using AFwalks.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AFwalks.API.Data
+{
+    public class AFwalksDbContext : DbContext
+    {
+        public AFwalksDbContext(DbContextOptions<AFwalksDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
--- AFwalks/AFwalks.API/Program.cs
+++ AFwalks/AFwalks.API/Program.cs
@@ -1,3 +1,6 @@
+using AFwalks.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AFwalksDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AFwalksConnection"));
+});
 
 var app = builder.Build();
 
